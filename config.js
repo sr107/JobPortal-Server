b7f1439d38fd4ae5cf3590e6a776c9c46879b47d
jeevan <bjeevanreddy45@gmail.com>
2020-05-05 18:43:00 +0530

    mongo atlas

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    secret: 'jeevan'
+    secret: 'jeevan',
+    MONGO_URI: 'mongodb+srv://jeevan:jeevan@cluster0-jdiwq.mongodb.net/jobsportal?retryWrites=true&w=majority'
+
 }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 const trueLog = require('true-log');
 const fs = require('fs');
+const config =require('./config');
 const readline = require('readline');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -15,7 +16,7 @@ const jobsrouter = require('./routers/jobs.router');
 //const authorization = require('./middlewares/middle');
 var cors= require('cors');
 app.use(cors({
-    origin:['http://localhost:4100','http://127.0.0.1:4100'],
+    origin:'*',
     credentials:true
   }));
   
@@ -52,40 +53,15 @@ app.use('/jobs', jobsrouter);
 
 //mongodb://localhost:27017/project
 
-mongoose.connect('mongodb+srv://jeevan:jeevan@cluster0-jdiwq.mongodb.net/project?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
+mongoose.connect(config.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (error, res) => {
     if (res) {
-        console.log('DB1 Connected successfully');
+        console.log('Mongo Atlas Connected successfully');
     }
     else {
-        console.log("Something error occured");
+        console.log("Something error occurred");
     }
 });
-
-// const arraylog=[];
-// var input=null;
-// const reader=readline.createInterface({input:
-//     fs.createReadStream(__dirname+'/log.txt')});
-// reader.on('line',line =>{
-//     arraylog.push(JSON.parse(line));
-// });
-// reader.on('close',d=>{
-//     arraylog.push(e=>console.log(e.action));
-// })
-// fs.readFile('log.txt', function(err, buf) {
-//     const a = buf.toString();
-//     let serachers=0;
-//     const arraylog=[];
-//     const b = a.split('}');
-//     for(let i = 0; i < b.length; i++){
-//         // if(b[i].url===""/public/addrecruiter"")
-//         // {
-//         //     serachers++;
-//         // }
-//         arraylog.push(JSON.parse(b[i]));
-//     }
-//     console.log(arraylog);
-// })
-
-app.listen(3000, function () {
-    console.log('Server runing on 3000 port');
+const port=process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log(`Server runing on ${port} port`);
 })
